5e4324b8af61db051bab37fa81002b6baaf52605
cavid32 <javidhal@code.edu.az>
2023-09-26 21:00:22 +0400

    sfvfv

---
+++ main.js
@@ -0,0 +1,6 @@
+const mebleg=document.getElementById("mebleg");
+const faizinputu=document.getElementById("faiz");
+const ayinputu=document.getElementById("ay");
+const priceinputu=document.getElementById("price");
+const orderinputu =document.getElementById("order");
+
