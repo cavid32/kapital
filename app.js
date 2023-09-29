3acdff7f08e5473f538fe2ddb69ac31efd63d318
cavid32 <javidhal@code.edu.az>
2023-09-30 00:34:01 +0400

    kapital-kredit-commit

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-const meblegInputu = document.getElementById('mebleg');
-const faizInputu = document.getElementById('faiz');
-const ayInputu = document.getElementById('ay');
-const ayliqOdenisXanasi = document.getElementById('ayliqOdenis');
+const mebleg = document.getElementById("mebleg");
+const faiz = document.getElementById("faiz");
+const ayliq = document.getElementById("ayliq");
+const umumiMebleg = document.getElementById("umumiMebleg");
+const order = document.getElementById("order");
+const isdediyinmebleg = document.getElementById("isdediyinmebleg");
+const LAbelFaiz=document.getElementById("LabelFaiz");
+const LabelAy=document.getElementById("LabelAy");
+const muddet=document.getElementById("muddet"); 
 
 
-let goturmekIstediyim = 0;
-let yekunFaizDaxilMebleg = 0;
-let yekunNetice = 0;
 
-meblegInputu.addEventListener('input', function () {
-    yekunFaizDaxilMebleg = Number(meblegInputu.value) + ((Number(meblegInputu.value) * Number(faizInputu.value)) / 100);
-    yekunNetice = yekunFaizDaxilMebleg / ayInputu.value;
-    ayliqOdenisXanasi.textContent = yekunNetice.toFixed(2)
-})
+let bankinfaizi = 0;
+let bankaodeyeceyim = 0;
+let ayliqodenis = 0;
+
+
+function kredit() {
+  isdediyinmebleg.innerText=mebleg.value;
+  LAbelFaiz.innerText=faiz.value;
+  LabelAy.innerText=ayliq.value
+  bankinfaizi = (Number(mebleg.value) * Number(faiz.value)) / 100;
+  bankaodeyeceyim = Number(mebleg.value) + Number(bankinfaizi);
+  ayliqodenis = Number(bankaodeyeceyim) / Number(ayliq.value);
+  console.log(bankinfaizi.toFixed(2));
+  console.log(bankaodeyeceyim.toFixed(2));
+  console.log(ayliqodenis.toFixed(2));
+
+  umumiMebleg.textContent=ayliqodenis;
+}
+
+
+
+
+
+
+document.getElementById("mebleg").addEventListener("input", kredit);
+document.getElementById("umumiMebleg").addEventListener("umumiMebleg.textContent",kredit);
+document.getElementById("ayliq").addEventListener("input",kredit);
+document.getElementById("faiz").addEventListener("input",kredit);
--- main.js
+++
@@ -1,6 +0,0 @@
-const mebleg=document.getElementById("mebleg");
-const faizinputu=document.getElementById("faiz");
-const ayinputu=document.getElementById("ay");
-const priceinputu=document.getElementById("price");
-const orderinputu =document.getElementById("order");
-
